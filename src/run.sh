a59651c1e7e9783d83f8ae6db087eb3e4386fb93
MitsuruFujiwara <fujiwara52jp@gmail.com>
2019-06-04 06:12:50 +0900

    LB#5 cv:0.639896

--- src/run.sh
+++ src/run.sh
@@ -5,8 +5,8 @@
 #python 004_queries_profiles.py
 #python 101_train_lgbm_queries.py
 #python 102_train_lgbm_queries_profiles.py
-python 005_aggregation.py
+#python 005_aggregation.py
 python 103_train_lgbm.py
-python 104_train_xgb.py
+#python 104_train_xgb.py
 python 201_blend.py
-python 102_train_lgbm_queries_profiles.py
+#python 102_train_lgbm_queries_profiles.py
